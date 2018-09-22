e7c48fd84afbb0d553edfb48be08afc94fbc20b9
Pawel Szymczak <picasso90@o2.pl>
2018-09-22 20:57:55 +0200

    Inicjalizacja repozytorium master

---
+++ FUNCTIONS/TEST.sql
@@ -0,0 +1,12 @@
+--------------------------------------------------------
+--  DDL for Function TEST
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE FUNCTION "TEST_MASTER"."TEST" 
+    return varchar2
+as
+begin
+    return 'tst';
+end;
+
+/
---
+++ init_repo.sh
@@ -0,0 +1,8 @@
+filename="Generate.sql"
+filename2="init_repo.sql"
+while read -r line
+do
+ str="line"
+ echo.%str%
+done < "$filename"
+read
